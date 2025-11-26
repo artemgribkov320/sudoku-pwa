7570e45935398dd9f2d155517d74bd26056bfb46
artemgribkov320 <196576297+artemgribkov320@users.noreply.github.com>
2025-11-26 02:09:04 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "sudoku-mobile-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./sudoku_mobile.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", evt => {
+  evt.respondWith(caches.match(evt.request).then(r => r || fetch(evt.request)));
+});
